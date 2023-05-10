ffc9595ab97ce89a9bef1125efc7954e68f7c2f0
MaximilianPokupec <maximilian.pokupec@gmail.com>
2023-05-10 16:18:03 +0200

    Added component terminModal.js , modified Activities.js

--- src/Pages/Activities.js
+++ src/Pages/Activities.js
@@ -1,6 +1,26 @@
+import {terminModal} from "../terminModal.js" 
+import React, { useState } from 'react';
+import { Container, Row, Col } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
 export const Activities = () => {
+    
+    const [modalShow, setModalShow] = React.useState(false);
+
     return (
-        <h1>Activities Page</h1>
+        
+        <Container>
+            <Row>
+                <Button onClick={() => setModalShow(true)}>Probaj</Button>
+                <terminModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                />
+            </Row>
+            <Row></Row>
+            <Row></Row>
+        </Container>
+        
     );
 }
 
