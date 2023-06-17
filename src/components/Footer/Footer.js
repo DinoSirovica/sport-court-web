6275644c2bdcea456b8cb6b6e1284f4c3b46503b
DinoSirovica <dino14sirovica@gmail.com>
2023-06-17 18:47:04 +0200

    Made small adjustments to the Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,34 +1,56 @@
 import {Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import {isAuthenticated} from "../../util/auth";
 
 export const Footer = () => {
-    return (
-        <footer className="footer">
-            <Container>
-                <Row className="justify-content-md-center">
-                    <Col md={6}>
-                        <h5 className="footer-title">O nama</h5>
-                        <p>Ovo je platforma namjenjena... </p>
-                    </Col>
-                    <Col md={6}>
-                        <h5 className="footer-title">Navigacija</h5>
-                        <ul className="list-unstyled">
+    if(isAuthenticated())
+        return (
+            <footer className="footer">
+                <Container>
+                    <Row className="justify-content-center text-center mb-5 p-1">
+                        <Col md={3}>
                             <Link className="link" to="/"> Naslovna </Link>
-
+                        </Col>
+                        <Col md={3}>
                             <Link className="link" to="/activities"> Aktivnosti </Link>
-
+                        </Col>
+                        <Col md={3}>
                             <Link className="link" to="/about-us"> O nama </Link>
+                        </Col>
+                        <Col md={3}>
+                            <Link className="link" to="/profile"> Profil </Link>
+                        </Col>
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md={6} className="text-center">
+                            <p>&copy; 2023 Sport Court. All Rights Reserved.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+        )
+    else
+        return (
+            <footer className="footer">
+                <Container>
+                    <Row className="justify-content-center text-center mb-5 p-1">
+                        <Col md={4}>
+                            <Link className="link" to="/"> Naslovna </Link>
+                        </Col>
+                        <Col md={4}>
+                            <Link className="link" to="/activities"> Aktivnosti </Link>
+                        </Col>
+                        <Col md={4}>
+                            <Link className="link" to="/about-us"> O nama </Link>
+                        </Col>
 
-                        </ul>
-                    </Col>
-
-                </Row>
-                <Row className="justify-content-md-center">
-                    <Col md={6} className="text-center">
-                        <p>&copy; 2023 Sport Court. All Rights Reserved.</p>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    )
+                    </Row>
+                    <Row className="justify-content-md-center">
+                        <Col md={6} className="text-center">
+                            <p>&copy; 2023 Sport Court. All Rights Reserved.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
+        )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,7 @@ import "../css/HomePage.css"
 import "../css/fonts.css"
 import {SportButton} from '../components/Sport/Sport';
 import {Link} from "react-router-dom";
+import {Footer} from "../components/Footer/Footer";
 
 const Column = ({icon, text, miniIcon, arrow}) => {
     return (
@@ -124,33 +125,7 @@ export const Home = () => {
                 </Row>
             </Container>
 
-            <footer className="footer">
-                <Container>
-                    <Row className="justify-content-md-center">
-                        <Col md={6}>
-                            <h5 className="footer-title">O nama</h5>
-                            <p>Ovo je platforma namjenjena </p>
-                        </Col>
-                        <Col md={6}>
-                            <h5 className="footer-title">Navigacija</h5>
-                            <ul className="list-unstyled">
-                                <Link className="link" to="/"> Naslovna </Link>
-
-                                <Link className="link" to="/activities"> Aktivnosti </Link>
-
-                                <Link className="link" to="/aboutUS"> O nama </Link>
-
-                            </ul>
-                        </Col>
-
-                    </Row>
-                    <Row className="justify-content-md-center">
-                        <Col md={6} className="text-center">
-                            <p>&copy; 2023 Sport Court. All Rights Reserved.</p>
-                        </Col>
-                    </Row>
-                </Container>
-            </footer>
+            <Footer></Footer>
         </div>
     );
 }
